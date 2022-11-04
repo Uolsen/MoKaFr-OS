647fa3f3f4e33d2d7713ca15b847265e837a3d7f
Lukas Frey <mail@lukasfrey.cz>
2022-11-04 09:03:41 +0100

    chore: added basic gitignore

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lukasfrey/Development/ZCU/OS/MoKaFr-OS
-# Build directory: /Users/lukasfrey/Development/ZCU/OS/MoKaFr-OS/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
