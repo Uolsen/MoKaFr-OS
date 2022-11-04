dee38976e5c3778beea30dd9e324ae2c7fa807ac
Lukas Frey <mail@lukasfrey.cz>
2022-11-04 09:11:04 +0100

    chore: added basic gitignore

---
+++ cmake-build-debug/CMakeFiles/Mokafr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mokafr.dir/main.cpp.o"
+  "CMakeFiles/Mokafr.dir/main.cpp.o.d"
+  "Mokafr"
+  "Mokafr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mokafr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
