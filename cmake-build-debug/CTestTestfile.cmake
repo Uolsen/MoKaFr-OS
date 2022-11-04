050410bad41143d8d33c99eda43f6c2481a0989a
stepanmocjak <stepan.mocjak@guava.cz>
2022-11-04 08:47:05 +0100

    init2

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/stepanmocjak/Development/University/OS/SP
+# Build directory: /Users/stepanmocjak/Development/University/OS/SP/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
