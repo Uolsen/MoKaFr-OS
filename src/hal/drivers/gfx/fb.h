dc9ae00f9613b7234d108820efb28c85da1401c7
Lukas Frey <mail@lukasfrey.cz>
2022-11-27 16:51:03 +0100

    feat: changed back to full HD, added clear function
    Signed-off-by: Lukas Frey <mail@lukasfrey.cz>

--- src/hal/drivers/gfx/fb.h
+++ src/hal/drivers/gfx/fb.h
@@ -10,5 +10,6 @@ void drawString(int32_t x, int32_t y, char *s, unsigned char attr);
 void drawRect(int32_t x1, int32_t y1, int32_t x2, int32_t y2, unsigned char attr, int32_t fill);
 void drawCircle(int32_t x0, int32_t y0, int32_t radius, unsigned char attr, int32_t fill);
 void drawLine(int32_t x1, int32_t y1, int32_t x2, int32_t y2, unsigned char attr);
+void fb_clear();
 
 #endif //MOKAFR_FB_H
