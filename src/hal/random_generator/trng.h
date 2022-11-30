81e650d43b73c7a33805587e477f213f960d5361
hanakali <hanikali@email.cz>
2022-11-30 17:53:31 +0100

    refactor: DEBUG_P

--- src/hal/random_generator/trng.h
+++ src/hal/random_generator/trng.h
@@ -2,13 +2,13 @@
 #define _TRNG_H
 #include "lib.h"
 
-#define RANDOM_BASE_ADDR    0xFE104000UL
+#define RANDOM_BASE_ADDR    0xfe104000UL
 
-#define RNG_CTRL            ((volatile uint64_t) (RANDOM_BASE_ADDR + 0x00UL))
-#define RNG_STATUS          ((volatile uint64_t) (RANDOM_BASE_ADDR + 0x04UL))
-#define RNG_DATA            ((volatile uint64_t) (RANDOM_BASE_ADDR + 0x08UL))
-#define RNG_FF_THRESHOLD    ((volatile uint64_t) (RANDOM_BASE_ADDR + 0x0cUL))
-#define RNG_INT_MASK        ((volatile uint64_t) (RANDOM_BASE_ADDR + 0x10UL))
+#define RNG_CTRL            ((volatile uint32_t*) (RANDOM_BASE_ADDR + 0x00UL))
+#define RNG_STATUS          ((volatile uint32_t*) (RANDOM_BASE_ADDR + 0x04UL))
+#define RNG_DATA            ((volatile uint32_t*) (RANDOM_BASE_ADDR + 0x08UL))
+#define RNG_FF_THRESHOLD    ((volatile uint32_t*) (RANDOM_BASE_ADDR + 0x0cUL))
+#define RNG_INT_MASK        ((volatile uint32_t*) (RANDOM_BASE_ADDR + 0x10UL))
 
 void init_rand();
 uint64_t rand(uint64_t min, uint64_t max);
